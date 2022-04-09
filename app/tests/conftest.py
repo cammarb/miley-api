36660e6e3c3f95d38a4c1ce05f1eae3c3bc971e6
Camila Martinez <camilamarb98@gmail.com>
2022-04-08 19:14:11 -0500

    add tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,10 +1,14 @@
 import pytest
+from os import environ
+from flask_migrate import upgrade
 from app.app import create_app
 
 
 @pytest.fixture
 def client():
+    environ['DATABASE_URL'] = 'sqlite://'
     app = create_app()
 
     with app.app_context():
+        upgrade()
         yield app.test_client()
