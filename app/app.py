39c1e86a4a489ae0bd16d7bda76b6c297888b549
Camila Martinez <camilamarb98@gmail.com>
2022-04-08 19:17:02 -0500

    add blueprints / sql config

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+from app.extensions.database import db, migrate
 
 from . import api
 from . import static_pages
+from . import songs
+from . import albums
 
 
 def create_app():
@@ -10,6 +13,7 @@ def create_app():
     app.config.from_object('app.config')
 
     register_blueprints(app)
+    register_extensions(app)
 
     return app
 
@@ -17,3 +21,10 @@ def create_app():
 def register_blueprints(app: Flask):
     app.register_blueprint(static_pages.routes.bp)
     app.register_blueprint(api.routes.bp)
+    app.register_blueprint(albums.routes.bp)
+    app.register_blueprint(songs.routes.bp)
+
+
+def register_extensions(app: Flask):
+    db.init_app(app)
+    migrate.init_app(app, db)
--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+from os import environ
 from dotenv import load_dotenv
 
 load_dotenv()
+
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
