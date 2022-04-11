a35395caf455eeaa6a562adbdcd5c34a5ab2c758
Camila Martinez <camilamarb98@gmail.com>
2022-04-10 23:04:50 -0500

    remove crud from model

--- app/songs/models.py
+++ app/songs/models.py
@@ -1,7 +1,7 @@
-from app.extensions.database import db, CRUD_mixing
+from app.extensions.database import db
 
 
-class Song(db.Model, CRUD_mixing):
+class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300))
     album_id = db.Column(db.Integer, db.ForeignKey(
