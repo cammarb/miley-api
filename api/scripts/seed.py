848b51d81d239f5b405896852559a9bd5aff7593
Camila Martinez <camilamarb98@gmail.com>
2022-04-11 20:10:27 -0500

    update seed file for testing

--- api/scripts/seed.py
+++ api/scripts/seed.py
@@ -1,15 +1,16 @@
 from api.app import create_app
 from api.models.songs import Song
 from api.models.albums import Album
+from api.models.writers import Writer
+from api.models.producers import Producer
+from api.models.song_writers import SongWriter
+from api.models.song_producers import SongProducer
+
 from api.extensions.database import db
 
 app = create_app()
 app.app_context().push()
 
-discography = {
-    "songs": "/api/songs",
-    "albums": "/api/albums"
-}
 
 # Albums
 total_songs = 0
@@ -21,6 +22,7 @@ albums = [
         "title": "Meet Miley Cyrus",
         "release_date": "2007-06-27",
         "tracklist": tracklist,
+        "total_lenght": "33:05"
         # [
         #     "See You Again",
         #     "East Northumberland High",
@@ -33,161 +35,70 @@ albums = [
         #     "Good and Broken",
         #     "I Miss You"
         # ],
-        "total_songs": total_songs,
-        "total_lenght": "33:05"
     }
 ]
 
 # Songs
 songs = [
     {
-        {
-            "id": "1",
-            "title": "See You Again",
-            "length": "3:10",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Armato",
-                "James"
-            ],
-            "producers": [
-                "Armato",
-                "James"
-            ]
-        },
-        {
-            "id": "2",
-            "title": "East Northumberland High",
-            "length": "3:24",
-            "album_id": 1,
-            "writers": [
-                "Armato",
-                "James",
-                "Samantha Jo Moore"
-            ],
-            "producers": [
-                "Armato",
-                "James"
-            ]
-        },
-        {
-            "id": "3",
-            "title": "Let's Dance",
-            "length": "3:03",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Armato",
-                "James"
-            ],
-            "producers": [
-                "Armato",
-                "James"
-            ]
-        },
-        {
-            "id": "4",
-            "title": "G.N.O. (Girl's Night Out)",
-            "length": "3:38",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Matthew Wilder",
-                "Tamara Dunn"
-            ],
-            "producers": [
-                "Wilder"
-            ]
-        },
-        {
-            "id": "5",
-            "title": "Right Here",
-            "length": "2:45",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Armato",
-                "James"
-            ],
-            "producers": [
-                "Armato",
-                "James"
-            ]
-        },
-        {
-            "id": "6",
-            "title": "As I Am",
-            "length": "3:46",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Shelley Peiken",
-                "Xandy Barry"
-            ],
-            "producers": [
-                "Xandy Barry",
-                "Wally Gagel"
-            ]
-        },
-        {
-            "id": "7",
-            "title": "Start All Over",
-            "length": "3:27",
-            "album_id": 1,
-            "writers": [
-                "Scott Cutler",
-                "Anne Preven",
-                "Fefe Dobson"
-            ],
-            "producers": [
-                "Annetenna"
-            ]
-        },
-        {
-            "id": "8",
-            "title": "Clear",
-            "length": "3:03",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Peiken",
-                "Xandy Barry"
-            ],
-            "producers": [
-                "Armato",
-                "James"
-            ]
-        },
-        {
-            "id": "9",
-            "title": "Good and Broken",
-            "length": "2:56",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Armato",
-                "James"
-            ],
-            "producers": [
-                "Armato",
-                "James"
-            ]
-        },
-        {
-            "id": "10",
-            "title": "I Miss You",
-            "length": "3:58",
-            "album_id": 1,
-            "writers": [
-                "Destiny Hope Cyrus",
-                "Brian Green",
-                "Wendi Foy Green"
-            ],
-            "producers": [
-                "B. Green"
-            ]
-        }
+        "id": "1",
+        "title": "See You Again",
+        "length": "3:10",
+        "album_id": 1
+    },
+    {
+        "id": "2",
+        "title": "East Northumberland High",
+        "length": "3:24",
+        "album_id": 1
+    },
+    {
+        "id": "3",
+        "title": "Let's Dance",
+        "length": "3:03",
+        "album_id": 1
+    },
+    {
+        "id": "4",
+        "title": "G.N.O. (Girl's Night Out)",
+        "length": "3:38",
+        "album_id": 1
+    },
+    {
+        "id": "5",
+        "title": "Right Here",
+        "length": "2:45",
+        "album_id": 1
+    },
+    {
+        "id": "6",
+        "title": "As I Am",
+        "length": "3:46",
+        "album_id": 1
+    },
+    {
+        "id": "7",
+        "title": "Start All Over",
+        "length": "3:27",
+        "album_id": 1
+    },
+    {
+        "id": "8",
+        "title": "Clear",
+        "length": "3:03",
+        "album_id": 1
+    },
+    {
+        "id": "9",
+        "title": "Good and Broken",
+        "length": "2:56",
+        "album_id": 1
+    },
+    {
+        "id": "10",
+        "title": "I Miss You",
+        "length": "3:58",
+        "album_id": 1
     }
 ]
 
@@ -196,9 +107,8 @@ for id, song in songs.items():
         id=id,
         title=song['title'],
         length=song['length'],
-        album_id=song['album_id'],
-        writers=song['writers'],
-        producers=song['producers'])
+        album_id=song['album_id']
+    )
     db.session.add(new_song)
 
 db.session.commit()
