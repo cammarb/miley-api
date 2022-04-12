fcd49dc81e16f5e7115244d207e78d96ab3f6a3d
Camila Martinez <camilamarb98@gmail.com>
2022-04-11 23:11:41 -0500

    update songs route

--- api/routes/songs.py
+++ api/routes/songs.py
@@ -1,8 +1,11 @@
 import json
 from flask import Blueprint, jsonify
-from datetime import datetime
 from api.models.albums import Album
+from api.models.producers import Producer
 from api.models.songs import Song
+from api.models.song_writers import SongWriter
+from api.models.song_producers import SongProducer
+from api.models.writers import Writer
 
 bp = Blueprint('songs', __name__)
 
@@ -14,18 +17,33 @@ def serialize_songs(songs):
         songs_list.append({
             'id': song.id,
             'title': song.title,
-            'album': Album.query.get(song.album_id).title
+            'length': song.length,
+            'album': Album.query.get(song.album_id).title,
         })
 
     return songs_list
 
 
 def serialize_song(song):
+    song_writers = SongWriter.query.all()
+    song_producers = SongProducer.query.all()
     song = {
         'id': song.id,
         'title': song.title,
-        'album_id': song.album_id
+        'album_title': Album.query.get(song.album_id).title,
+        'length': song.length,
+        'writers': [],
+        'producers': []
     }
+    for song_writer in song_writers:
+        if song_writer.song_id == song['id']:
+            song['writers'].append(
+                Writer.query.get(song_writer.writer_id).name)
+
+    for song_producer in song_producers:
+        if song_producer.song_id == song['id']:
+            song['producers'].append(
+                Producer.query.get(song_producer.producer_id).name)
 
     return song
 
