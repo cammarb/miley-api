4d5257d0f9d7fa73484ef68bc0a6aefff41f2e67
Camila Martinez <camilamarb98@gmail.com>
2022-04-11 20:40:52 -0500

    update tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -6,9 +6,7 @@ from api.app import create_app
 
 @pytest.fixture
 def client():
-    environ['DATABASE_URL'] = 'sqlite://'
     app = create_app()
 
     with app.app_context():
-        upgrade()
         yield app.test_client()
--- api/tests/routes/test_albums.py
+++ api/tests/routes/test_albums.py
@@ -1,4 +1,4 @@
 def test_index_success(client):
     # Page loads
-    response = client.get('/')
+    response = client.get('/api/albums')
     assert response.status_code == 200
