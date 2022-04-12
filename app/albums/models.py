969f05cf69426dede8ab87c75186e57a1fb8218a
Camila Martinez <camilamarb98@gmail.com>
2022-04-11 19:09:33 -0500

    Refactor of models

--- app/albums/models.py
+++
@@ -1,9 +0,0 @@
-from app.extensions.database import db
-
-
-class Album(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300))
-    release_date = db.Column(
-        db.Date(), nullable=False)
-    songs = db.relationship('Song', backref='album', lazy=True)
--- app/songs/models.py
+++
@@ -1,8 +0,0 @@
-from app.extensions.database import db
-
-
-class Song(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300))
-    album_id = db.Column(db.Integer, db.ForeignKey(
-        'album.id'))
