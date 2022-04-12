cced5e23e7226471406572f63d4fd48b31eddee9
Camila Martinez <camilamarb98@gmail.com>
2022-04-11 21:26:35 -0500

    add test for scripts

---
+++ api/tests/scripts/test_scripts.py
@@ -0,0 +1,65 @@
+songs = [
+    {
+        "id": "1",
+        "title": "See You Again",
+        "length": "3:10",
+        "album_id": 1
+    },
+    {
+        "id": "2",
+        "title": "East Northumberland High",
+        "length": "3:24",
+        "album_id": 1
+    },
+    {
+        "id": "3",
+        "title": "Let's Dance",
+        "length": "3:03",
+        "album_id": 1
+    },
+    {
+        "id": "4",
+        "title": "G.N.O. (Girl's Night Out)",
+        "length": "3:38",
+        "album_id": 1
+    },
+    {
+        "id": "5",
+        "title": "Right Here",
+        "length": "2:45",
+        "album_id": 1
+    },
+    {
+        "id": "6",
+        "title": "As I Am",
+        "length": "3:46",
+        "album_id": 1
+    },
+    {
+        "id": "7",
+        "title": "Start All Over",
+        "length": "3:27",
+        "album_id": 1
+    },
+    {
+        "id": "8",
+        "title": "Clear",
+        "length": "3:03",
+        "album_id": 1
+    },
+    {
+        "id": "9",
+        "title": "Good and Broken",
+        "length": "2:56",
+        "album_id": 1
+    },
+    {
+        "id": "10",
+        "title": "I Miss You",
+        "length": "3:58",
+        "album_id": 1
+    }
+]
+
+for song in songs:
+    print(song['id'], song['title'])
