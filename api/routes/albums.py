fd8c78731e8c10a6c3c999cb5469159727d1d578
Camila Martinez <camilamarb98@gmail.com>
2022-04-12 19:16:43 -0500

    remove resializer from views

--- api/routes/albums.py
+++ api/routes/albums.py
@@ -1,54 +1,23 @@
 import json
-from flask import Blueprint, jsonify
-from datetime import datetime
+from flask import Blueprint
 from api.models.albums import Album
-from api.models.songs import Song
+from api.serializer.serializer import serialize_albums
 
 bp = Blueprint('albums', __name__)
-
-
-def serialize_albums(albums):
-    albums_list = []
-
-    for album in albums:
-        albums_list.append({
-            'id': album.id,
-            'title': album.title,
-            'release_date': datetime.strftime(album.release_date, '%Y-%m-%d'),
-            'total_length': album.total_length,
-        })
-
-    return albums_list
-
-
-def serialize_album(album):
-    songs = Song.query.all()
-
-    album = {
-        'id': album.id,
-        'title': album.title,
-        'release_date': datetime.strftime(album.release_date, '%Y-%m-%d'),
-        'total_length': album.total_length,
-        'tracklist': []
-    }
-    for song in songs:
-        if song.album_id == album['id']:
-            album['tracklist'].append(song.title)
-
-    return album
 
 
 @bp.get('/api/albums/')
 @bp.get('/api/albums')
 def get_albums():
     albums = Album.query.all()
-    return jsonify(serialize_albums(albums))
+    return json.dumps(serialize_albums(albums, 0))
 
 
 @bp.get('/api/albums/<int:id>/')
 @bp.get('/api/albums/<int:id>')
 def album(id):
-    album = Album.query.get(id)
-    return jsonify(
-        serialize_album(album)
+    albums = Album.query.all()
+    album_id = Album.query.get(id)
+    return json.dumps(
+        serialize_albums(albums, album_id)
     )
--- api/routes/songs.py
+++ api/routes/songs.py
@@ -1,64 +1,23 @@
 import json
-from flask import Blueprint, jsonify
-from api.models.albums import Album
-from api.models.producers import Producer
+from flask import Blueprint
 from api.models.songs import Song
-from api.models.song_writers import SongWriter
-from api.models.song_producers import SongProducer
-from api.models.writers import Writer
+from api.serializer.serializer import serialize_songs
 
 bp = Blueprint('songs', __name__)
-
-
-def serialize_songs(songs):
-    songs_list = []
-
-    for song in songs:
-        songs_list.append({
-            'id': song.id,
-            'title': song.title,
-            'length': song.length,
-            'album': Album.query.get(song.album_id).title,
-        })
-
-    return songs_list
-
-
-def serialize_song(song):
-    song_writers = SongWriter.query.all()
-    song_producers = SongProducer.query.all()
-    song = {
-        'id': song.id,
-        'title': song.title,
-        'album_title': Album.query.get(song.album_id).title,
-        'length': song.length,
-        'writers': [],
-        'producers': []
-    }
-    for song_writer in song_writers:
-        if song_writer.song_id == song['id']:
-            song['writers'].append(
-                Writer.query.get(song_writer.writer_id).name)
-
-    for song_producer in song_producers:
-        if song_producer.song_id == song['id']:
-            song['producers'].append(
-                Producer.query.get(song_producer.producer_id).name)
-
-    return song
 
 
 @bp.get('/api/songs/')
 @bp.get('/api/songs')
 def get_songs():
     songs = Song.query.all()
-    return jsonify(serialize_songs(songs))
+    return json.dumps(serialize_songs(songs, 0))
 
 
 @bp.get('/api/songs/<int:id>/')
 @bp.get('/api/songs/<int:id>')
 def song(id):
-    song = Song.query.get(id)
-    return jsonify(
-        serialize_song(song)
+    song_id = Song.query.get(id)
+    songs = Song.query.all()
+    return json.dumps(
+        serialize_songs(songs, song_id)
     )
