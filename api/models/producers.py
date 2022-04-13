95d8922e8a3d883442139a3da4aa6355cb393a49
Camila Martinez <camilamarb98@gmail.com>
2022-04-12 19:15:42 -0500

    remove producer model

--- api/models/producers.py
+++
@@ -1,8 +0,0 @@
-from api.extensions.database import db
-
-
-class Producer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    song_producers = db.relationship(
-        'SongProducer', backref='producer', lazy=True)
