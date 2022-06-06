2832b8ab3719b2bf4f37f23c42f99ae0e971bfcc
Camila Martinez <camilamarb98@gmail.com>
2022-06-06 16:31:04 -0500

    remove old models

--- api/models/song_writers.py
+++
@@ -1,8 +0,0 @@
-from api.extensions.database import db, CRUD_mixing
-
-
-class SongWriter(db.Model, CRUD_mixing):
-    song_id = db.Column(db.Integer, db.ForeignKey(
-        'song.id'), primary_key=True)
-    writer_id = db.Column(db.Integer, db.ForeignKey(
-        'writer.id'), primary_key=True)
