e329dab5e39619174e7a72455095244e60e1b822
Camila Martinez <camilamarb98@gmail.com>
2022-06-06 16:31:31 -0500

    remove crud option

--- api/extensions/database.py
+++ api/extensions/database.py
@@ -3,15 +3,3 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
-
-
-class CRUD_mixing():
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-        return
