53ae00a5587997a606b11a547009cfe605db58aa
Camila Martinez <camilamarb98@gmail.com>
2022-06-21 13:53:46 -0500

    add single song route

--- api/routes/songs.py
+++ api/routes/songs.py
@@ -13,11 +13,11 @@ def get_songs():
     return jsonify(serialize_songs(songs, 0))
 
 
-# @bp.get('/api/songs/<int:id>/')
-# @bp.get('/api/songs/<int:id>')
-# def song(id):
-#     song_id = Song.query.get(id)
-#     songs = Song.query.all()
-#     return json.dumps(
-#         serialize_songs(songs, song_id)
-#     )
+@bp.get('/api/songs/<int:id>/')
+@bp.get('/api/songs/<int:id>')
+def song(id):
+    song_id = Song.query.get(id)
+    songs = Song.query.all()
+    return jsonify(
+        serialize_songs(songs, song_id)
+    )
