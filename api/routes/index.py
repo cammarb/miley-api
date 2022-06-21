7691820e0680a9f5760e8e5b187db364bb7bf47f
Camila Martinez <camilamarb98@gmail.com>
2022-06-21 14:17:28 -0500

    add redirect

--- api/routes/index.py
+++ api/routes/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect, url_for
 import socket
 
 host = socket. getfqdn()
@@ -7,6 +7,10 @@ bp = Blueprint('api', __name__)
 
 
 @bp.get('/')
+def std_index():
+    return redirect(url_for('api.index'))
+
+
 @bp.get('/api/')
 @bp.get('/api')
 def index():
