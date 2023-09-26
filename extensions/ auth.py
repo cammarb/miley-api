21b23fd9ae54af70d243ae8467e7e1f5d9c6dcea
Camila Martinez <camilamarb98@gmail.com>
2023-09-26 03:03:44 +0200

    Add auth

--- extensions/ auth.py
+++ extensions/ auth.py
@@ -1,7 +1,8 @@
 from flask_login import LoginManager
-from app.models.user import User
+from app.model.users import User
 
 login_manager = LoginManager()
+
 
 @login_manager.user_loader
 def load_user(user_id):
