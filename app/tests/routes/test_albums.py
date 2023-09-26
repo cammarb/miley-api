e551c603b737a3a48618285046b5267cbd86dfba
Camila Martinez <camilamarb98@gmail.com>
2023-09-26 03:05:57 +0200

    Rename 'routes' to 'view'

--- app/tests/routes/test_albums.py
+++
@@ -1,4 +0,0 @@
-def test_index_success(client):
-    # Page loads
-    response = client.get('/api/albums')
-    assert response.status_code == 200
--- app/tests/routes/test_index.py
+++
@@ -1,4 +0,0 @@
-def test_index_success(client):
-    # Page loads
-    response = client.get('/api')
-    assert response.status_code == 200
